6dcb082408eb19273db67541492702e9f186fcad
snehakn356 <snehakn291@gmail.com>
2025-03-12 20:01:46 +0530

    Create Num.py

---
+++ Num.py
@@ -0,0 +1,8 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("GCD:", gcd(num1, num2))
