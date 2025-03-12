9ff8de48bd9780afcc98fd23a121d591ab55968e
snehakn356 <snehakn291@gmail.com>
2025-03-12 15:28:15 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+num = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
+
+
+print("Sneha EC067")
