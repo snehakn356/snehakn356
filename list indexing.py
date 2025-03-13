38ba6be9ff70967cb5b499bed6694c27fc1f53b7
snehakn356 <snehakn291@gmail.com>
2025-03-13 15:08:31 +0530

    Create list indexing.py

---
+++ list indexing.py
@@ -0,0 +1,7 @@
+numbers =[1,2,3,4,5]
+first_element = numbers[0]
+last_element = numbers[-1]
+middle_element = numbers [len(numbers)//2]
+print("first element:",first_element)
+print("last element:",last_element)
+print("middle element:",middle_element)
