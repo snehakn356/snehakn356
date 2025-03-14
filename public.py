712e91d4ede9742e59f339a606b5f7025ffc6bf5
snehakn356 <snehakn291@gmail.com>
2025-03-14 10:43:32 +0530

    Create public.py

---
+++ public.py
@@ -0,0 +1,10 @@
+class sneha:
+  def_init_(self):
+    self.pub = "ok"
+    self._suresh="not ok"
+    def chandu_private(self):
+      print(self.__suresh)
+      megha=sneha()
+     print(megha.pub)
+      megha.chandu_private()
+
